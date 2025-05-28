0a4f6464957a19299e9ed47442286287f359bee5
Tarun Janapati <tarunsai.janapati@slu.edu>
2025-05-27 23:03:27 -0500

    coop policy

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,23 @@ const nextConfig: NextConfig = {
     // Disable ESLint during builds
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups'
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'credentialless'
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
